d3b8002bc51a204cbb562bed7cbf382fa219d23e
iaroslav_shavrov <iaroslav_shavrov@practice.relex.ru>
2024-07-06 21:34:36 +0300

    первый коммит

---
+++ frontend/src/common/styles/GlobalStyles.tsx
@@ -0,0 +1,98 @@
+import { Global, css } from "@emotion/react";
+import styled from "@emotion/styled";
+
+export const Container = styled("div")`
+  max-width: calc(1250px + 50px * 2);
+  padding-inline: 50px;
+  margin-inline: auto;
+`;
+
+export const GlobalStyles = () => {
+  return (
+    <>
+      <Global
+        styles={css`
+          *,
+          *::before,
+          *::after {
+            margin: 0;
+            padding: 0;
+            box-sizing: border-box;
+          }
+
+          :where(ul, ol):where([class]) {
+            padding-left: 0;
+          }
+
+          body,
+          :where(blockquote, figure):where([class]) {
+            margin: 0;
+          }
+
+          :where(h1, h2, h3, h4, h5, h6, p, ul, ol, dl):where([class]) {
+            margin-block: 0;
+          }
+
+          :where(dd[class]) {
+            margin-left: 0;
+          }
+
+          :where(fieldset[class]) {
+            margin-left: 0;
+            padding: 0;
+            border: none;
+          }
+
+          :where(ul[class]) {
+            list-style: none;
+          }
+
+          p {
+            margin-block: 0;
+          }
+
+          p:where(:not([class]):not(:last-child)) {
+            margin-bottom: var(--paragraphMarginBottom);
+          }
+
+          img {
+            display: block;
+            max-width: 100%;
+          }
+
+          input,
+          textarea,
+          select,
+          button {
+            font: inherit;
+          }
+
+          html {
+            height: 100%;
+            scroll-behavior: smooth;
+          }
+
+          body {
+            min-height: 100%;
+            line-height: 1.5;
+          }
+
+          svg *[fill] {
+            fill: currentColor;
+          }
+          svg *[stroke] {
+            stroke: currentColor;
+          }
+
+          svg * {
+            transition-property: fill, stroke;
+          }
+
+
+          body {
+          }
+        `}
+      />
+    </>
+  );
+};
