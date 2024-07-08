ee7f67b15c5833772a31c6006a0fad0fe1414439
ArtemH2004 <159913643+ArtemH2004@users.noreply.github.com>
2024-07-08 23:50:51 +0300

    fonts. Абсолютные пути и шрифты

---
+++ frontend/src/common/styles/styleConstants.ts
@@ -0,0 +1,27 @@
+export const colors = {
+    //White
+    whiteTotal: "rgba(255, 255, 255, 1)",
+    whiteBackground: "rgba(245, 245, 245, 1)",
+  
+    //Gray
+    grayBorder: "rgba(238, 238, 238, 1)",
+  
+    //Black
+    blackTotal: "rgba(0, 0, 0, 1)",
+  };
+  
+  export const shadows = {
+    defaultShadow: "0 0 5px 0 rgba(0, 0, 0, 0.25)",
+  };
+  
+  export const borders = {
+    defaultBorder: `1px solid ${colors.grayBorder}`,
+    defaultBorderRadius: "20px",
+  };
+  
+  export const transitions = {
+      mediumTransition: 'all 0.5s ease 0s',
+      fastTransition: 'all 0.2s ease 0s',
+      lowTransition: 'all 0.8s ease 0s',
+    };
+  
